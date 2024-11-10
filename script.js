20cc79da09eed37a3497afa39c70653df72e6db0
Erickson Dave Geroleo <ericksongeroleo@gmail.com>
2024-11-11 05:38:21 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// const greetings = document.querySelector(".greetings");
+// const startBtn = document.getElementById("StartBtn");
+// const heroSection = document.querySelector(".hero_section");
+
+// // Hide the sections initially
+// [heroSection].forEach(section => section.style.display = "none");
+
+// startBtn.onclick = function() {
+//     greetings.style.display = "none";
+//     showHeroSection();
+
+//     // setTimeout(() => {
+//     //     greetings.style.display = "none";
+//     //     heroSection.style.display = "flex"; 
+//     //     heroSection.style.animation = "slideup 1s ease forwards";  
+//     // }, 1000);  
+// };
+
+
+
+
+
+// function showHeroSection() {
+//     // heroSection.style.display = "flex";  
+//     // const newDiv = document.createElement("div");
+//     // newDiv.classList.add('d-flex-col')  
+//     // newDiv.textContent = "Welcome to the Hero Section";  
+//     // heroSection.appendChild(newDiv);  
+// }
